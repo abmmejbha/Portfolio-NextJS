8b51bec37fd11467795a60618cebd06dfb6b3f53
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-05-25 13:02:43 +0600

    added description to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <>
-      <div>
-        
+      <div className="flex justify-between items-center px-8 py-20 min-h-screen">
+        <div className="flex flex-col gap-4 w-1/2">
+        {/* Left */}
+          <p>Hello, It is me</p>
+          <h1 className="text-4xl font-bold text-white">ABM Mejbha</h1>
+          <p>And I am a Web Devloper</p>
+          <p>Description</p>
+          <div>Social icons</div>
+          <button>Download CV</button>
+        </div>
+        <div>
+        {/* Right */}
+          <img src="" alt="" />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
