7f65bc21b2fcdd6d5db2d6610b6778ebf7226fd2
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-05-26 00:28:10 +0600

    made portfolio card

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,74 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const Portfolio = () => {
+export default function Portfolio() {
   return (
-    <>
-      <div>
-        Portfolio
+    <div className="px-8 py-20 gap-8 min-h-screen justify-between grid grid-cols-3 ">
+      {/* Currency Converter */}
+      <div className="flex flex-col gap-2 bg-slate-300 p-6  shadow-lg rounded-2xl hover:bg-white transition-all ">
+        <Image src="" alt="" />
+        <h3 className="text-2xl font-bold text-slate-900 mb-6 text-center">Currency Converter</h3>
+        <p className="text-sm text-slate-500 leading-relaxed">Description</p>
+        <div className="flex flex-col">
+          <a href="">Live Demo</a>
+          <a href="">Github Link</a>
+        </div>
       </div>
-    </>
-  )
-}
 
-export default Portfolio
+      {/* Movie search App */}
+      <div className="flex flex-col gap-2 bg-slate-300 p-6  shadow-lg rounded-2xl hover:bg-white transition-all ">
+        <Image src="" alt="" />
+        <h3 className="text-2xl font-bold text-slate-900 mb-6 text-center">Movie Search App</h3>
+        <p className="text-sm text-slate-500 leading-relaxed">Description</p>
+        <div className="flex flex-col"> 
+          <a href="">Live Demo</a>
+          <a href="">Github Link</a>
+        </div>
+      </div>
+
+      {/* Portfolio */}
+      <div className="flex flex-col gap-2 bg-slate-300 p-6  shadow-lg rounded-2xl hover:bg-white transition-all ">
+        <Image src="" alt="" />
+        <h3 className="text-2xl font-bold text-slate-900 mb-6 text-center">Portfolio</h3>
+        <p className="text-sm text-slate-500 leading-relaxed">Description</p>
+        <div className="flex flex-col">
+          <a href="">Live Demo</a>
+          <a href="">Github Link</a>
+        </div>
+      </div>
+
+      {/* User Management System */}
+      <div className="flex flex-col gap-2 bg-slate-300 p-6  shadow-lg rounded-2xl hover:bg-white transition-all ">
+        <Image src="" alt="" />
+        <h3 className="text-2xl font-bold text-slate-900 mb-6 text-center">User Management System</h3>
+        <p className="text-sm text-slate-500 leading-relaxed">Description</p>
+        <div className="flex flex-col">
+          <a href="">Live Demo</a>
+          <a href="">Github Link</a>
+        </div>
+      </div>
+
+      {/* Hospital Management System */}
+      <div className="flex flex-col gap-2 bg-slate-300 p-6  shadow-lg rounded-2xl hover:bg-white transition-all ">
+        <Image src="" alt="" />
+        <h3 className="text-2xl font-bold text-slate-900 mb-6 text-center">Hospital Management System (C)</h3>
+        <p className="text-sm text-slate-500 leading-relaxed">Description</p>
+        <div className="flex flex-col">
+          <a href="">Live Demo</a>
+          <a href="">Github Link</a>
+        </div>
+      </div>
+
+      {/* Snake Game */}
+      <div className="flex flex-col gap-2 bg-slate-300 p-6  shadow-lg rounded-2xl hover:bg-white transition-all ">
+        <Image src="" alt="" />
+        <h3 className="text-2xl font-bold text-slate-900 mb-6 text-center">Snake Game (C++)</h3>
+        <p className="text-sm text-slate-500 leading-relaxed">Description</p>
+        <div className="flex flex-col">
+          <a href="">Live Demo</a>
+          <a href="">Github Link</a>
+        </div>
+      </div>
+    </div>
+  );
+}
