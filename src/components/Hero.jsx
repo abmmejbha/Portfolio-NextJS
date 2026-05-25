f243b098512d3d53b96e066cd3159852183bbfd1
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-05-25 13:35:15 +0600

    completed the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <>
       <div className="flex justify-between items-center px-8 py-20 min-h-screen">
         <div className="flex flex-col gap-4 w-1/2">
-        {/* Left */}
-          <p>Hello, It is me</p>
-          <h1 className="text-4xl font-bold text-white">ABM Mejbha</h1>
-          <p>And I am a Web Devloper</p>
-          <p>Description</p>
+          {/* Left */}
+          <p className="text-lg text-blue-600 font-medium">Hello, It is me</p>
+          <h1 className="text-4xl font-bold text-slate-900">ABM Mejbha</h1>
+          <p className="text-2xl font-semibold text-slate-600">
+            And I am a Web Developer
+          </p>
+          <p className="text-sm text-slate-500 leading-relaxed">Description</p>
           <div>Social icons</div>
-          <button>Download CV</button>
+          <button className="bg-blue-600 px-6 py-3 rounded-lg text-white font-semibold hover:bg-blue-700 transition-colors cursor-pointer">
+            Download CV
+          </button>
         </div>
-        <div>
-        {/* Right */}
-          <img src="" alt="" />
+        <div className="w-1/2 flex justify-center">
+          {/* Right */}
+          <Image
+            className="object-cover w-72 "
+            src="/images/me.PNG"
+            alt="ABM Mejbha"
+            width={300}
+            height={300}
+          />
         </div>
       </div>
     </>
