acac52a1d2bd72156b9e91d06608abb72f2cb072
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-05-30 23:30:21 +0600

    refactor: update Navbar component styles for improved theming and consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,11 @@ export default function Navbar() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <nav className="fixed top-0 w-full z-50 bg-[#c8e0f0] dark:bg-[#364958]  border-b border-[#38a3a5] dark:border-[#3b6064] shadow-md">
+    <nav className="fixed top-0 w-full z-50 bg-bg-primary border-b border-border-color shadow-md">
       {/* main navigation bar container */}
       <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-4">
         <Link
-          className="text-xl font-bold text-[#22577a] dark:text-[#87bba2]"
+          className="text-xl font-bold text-text-primary"
           href="/"
         >
           ABM Mejbha
@@ -22,32 +22,32 @@ export default function Navbar() {
         {/* Desktop Navigation */}
         <div className="hidden md:flex gap-4">
           <Link
-            className="text-sm font-medium text-[#22577a] dark:text-[#05bd95] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-sm font-medium text-text-primary hover:text-text-secondary transition-colors"
             href="/"
           >
             Home
           </Link>
           <Link
-            className="text-sm font-medium text-[#22577a] dark:text-[#05bd95] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-sm font-medium text-text-primary hover:text-text-secondary transition-colors"
             href="#about"
           >
             About
           </Link>
 
           <Link
-            className="text-sm font-medium text-[#22577a] dark:text-[#05bd95] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-sm font-medium text-text-primary hover:text-text-secondary transition-colors"
             href="#services"
           >
             Services
           </Link>
           <Link
-            className="text-sm font-medium text-[#22577a] dark:text-[#05bd95] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-sm font-medium text-text-primary hover:text-text-secondary transition-colors"
             href="#portfolio"
           >
             Portfolio
           </Link>
           <Link
-            className="text-sm font-medium text-[#22577a] dark:text-[#05bd95] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-sm font-medium text-text-primary hover:text-text-secondary transition-colors"
             href="#contact"
           >
             Contact
@@ -74,37 +74,37 @@ export default function Navbar() {
 
       {/* Mobile Dropdown menu */}
       {isOpen && (
-        <div className="z-60 md:hidden flex flex-col gap-4 p-4 bg-[#c8e0f0] dark:bg-[#364958] shadow-lg">
+        <div className="z-60 md:hidden flex flex-col gap-4 p-4 bg-bg-primary shadow-lg">
           <Link
-            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-text-secondary transition-colors"
             onClick={() => setIsOpen(false)}
             href="/"
           >
             Home
           </Link>
           <Link
-            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-text-secondary transition-colors"
             onClick={() => setIsOpen(false)}
             href="#about"
           >
             About
           </Link>
           <Link
-            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-text-secondary transition-colors"
             onClick={() => setIsOpen(false)}
             href="#services"
           >
             Services
           </Link>
           <Link
-            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-text-secondary transition-colors"
             onClick={() => setIsOpen(false)}
             href="#portfolio"
           >
             Portfolio
           </Link>
           <Link
-            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-[#38a3a5] dark:hover:text-[#55828b] transition-colors"
+            className="text-[#22577a] dark:text-[#c9e5ca] hover:text-text-secondary transition-colors"
             onClick={() => setIsOpen(false)}
             href="#contact"
           >
