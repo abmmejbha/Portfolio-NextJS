cdd4929c153a70e90f1d3dfbbdadfbc0bdf81064
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-06-08 10:55:12 +0600

    updated contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,38 +2,38 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div id="contact" className="min-h-screen bg-slate-300">
-      <h1 className="pt-20 pb-12 font-bold text-3xl text-center text-slate-900 ">
+    <div id="contact" className="min-h-screen bg-bg-primary">
+      <h1 className="pt-20 pb-12 font-bold text-3xl text-center text-text-primary ">
         Contact <span className="text-blue-900">Me!</span>
       </h1>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 px-8 py-4">
         <input
-          className="bg-white text-slate-900 w-full px-4 py-4 border border-slate-300 rounded-2xl outline-none focus:border-blue-500 transition-colors"
+          className="bg-white text-text-primary w-full px-4 py-4 border border-border-color rounded-2xl outline-none focus:border-accent transition-colors"
           type="text"
           placeholder="Enter Name"
         />
         <input
-          className="bg-white text-slate-900 w-full px-4 py-4 border border-slate-300 rounded-2xl outline-none focus:border-blue-500 transition-colors"
+          className="bg-white text-text-primary w-full px-4 py-4 border border-border-color rounded-2xl outline-none focus:border-accent transition-colors"
           type="text"
           placeholder="Enter Email"
         />
         <input
-          className="bg-white text-slate-900 w-full px-4 py-4 border border-slate-300 rounded-2xl outline-none focus:border-blue-500 transition-colors"
+          className="bg-white text-text-primary w-full px-4 py-4 border border-border-color rounded-2xl outline-none focus:border-accent transition-colors"
           type="text"
           placeholder="Phone number"
         />
         <input
-          className="bg-white text-slate-900 w-full px-4 py-4 border border-slate-300 rounded-2xl outline-none focus:border-blue-500 transition-colors"
+          className="bg-white text-text-primary w-full px-4 py-4 border border-border-color rounded-2xl outline-none focus:border-accent transition-colors"
           type="text"
           placeholder="Email Subject"
         />
       </div>
       <div className="px-8 py-4 flex flex-col items-center gap-6">
         <textarea
-          className="w-full h-32 bg-white rounded-2xl text-slate-900 p-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
+          className="w-full h-32 bg-white rounded-2xl text-text-primary p-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
           placeholder="Your Message"
         />
-        <button className=" bg-blue-600 hover:bg-blue-800 rounded-lg px-20 py-2 text-white font-semibold">
+        <button className=" bg-accenthover:bg-bg-secondary rounded-lg px-20 py-2 text-white font-semibold">
           Send Message
         </button>
       </div>
