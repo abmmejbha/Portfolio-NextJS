1d98def39ffc060bba67e32777c85ba198594c6d
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-06-19 21:57:15 +0600

    feat: update font variables in globals.css and layout.js for improved consistency

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,18 @@
-import {Inter} from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { ThemeProvider } from "next-themes"
 import "./globals.css"
 
 const inter = Inter({
   subsets: ['latin'], 
   weight: ['300', '400', '600', '700'],
-  variable: '--font-inter'
+  variable: '--font-inter', // সিএসএস-এ ব্যবহারের জন্য এই নামটা জরুরি
 })
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.variable}>
+      {/* inter.variable এবং font-sans দুটোই একসাথে দিন */}
+      <body className={`${inter.variable} font-sans`}>
         <ThemeProvider attribute="class">
           {children}
         </ThemeProvider>
